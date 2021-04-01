bcfe19820e907031a9db99801c60cc57944c3d2e
Felix Swinkes <tgifelix@icloud.com>
2021-04-01 20:34:50 +0200

    whitespace

--- src/_includes/script.js
+++ src/_includes/script.js
@@ -12,7 +12,7 @@
     }
   })();
 
-  // HotJar
+// HotJar
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:2076898,hjsv:6};
@@ -21,3 +21,4 @@
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
